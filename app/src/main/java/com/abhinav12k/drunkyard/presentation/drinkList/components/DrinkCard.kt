2e940e12c0ddae67e7bb9bbc18d2ba53820005d1
Abhinav <abhinav@indwealth.in>
2022-08-27 23:50:32 +0530

    font issuec

--- app/src/main/java/com/abhinav12k/drunkyard/presentation/drinkList/components/DrinkCard.kt
+++ app/src/main/java/com/abhinav12k/drunkyard/presentation/drinkList/components/DrinkCard.kt
@@ -7,13 +7,11 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,7 +46,8 @@ fun DrinkCard(
             Text(
                 modifier = Modifier.padding(8.dp),
                 text = drinkCard.drinkName,
-                style = MaterialTheme.typography.h2,
+                style = MaterialTheme.typography.caption,
+                color = Color.Black,
                 textAlign = TextAlign.Center,
             )
         }
