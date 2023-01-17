8fe7885e15a37e724a3637874f30fd04ee25f3e5
nathanmeade <nathan.r.meade@gmail.com>
2023-01-16 21:00:12 -0500

    The search bar now displays an 'x' to clear the search text once there has actually been some input. The keyboard now disappears when the checkmark is selected on the users keyboard after typing their search text.

--- app/src/main/java/com/abhinav12k/drunkyard/presentation/drinkList/components/SearchBar.kt
+++ app/src/main/java/com/abhinav12k/drunkyard/presentation/drinkList/components/SearchBar.kt
@@ -1,18 +1,25 @@
 package com.abhinav12k.drunkyard.presentation.drinkList.components
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.abhinav12k.drunkyard.R
@@ -20,6 +27,7 @@ import com.abhinav12k.drunkyard.common.clearFocusOnKeyboardDismiss
 import com.abhinav12k.drunkyard.presentation.ui.theme.DrunkyardTheme
 import com.abhinav12k.drunkyard.presentation.ui.theme.SearchBarDarkBackground
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchBar(
     @StringRes placeHolder: Int,
@@ -27,6 +35,8 @@ fun SearchBar(
     value: String,
     onValueChange: (String) -> Unit
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
     TextField(
         leadingIcon = {
             Icon(imageVector = Icons.Default.Search, contentDescription = null)
@@ -49,7 +59,24 @@ fun SearchBar(
         singleLine = true,
         onValueChange = onValueChange,
         modifier = modifier
-            .fillMaxWidth()
+            .fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                keyboardController?.hide()
+                focusManager.clearFocus()
+            }),
+        trailingIcon = {
+            when { value.isNotEmpty() ->
+                Icon(Icons.Default.Clear,
+                    contentDescription = "clear text",
+                    modifier = Modifier
+                        .clickable {
+                            onValueChange("")
+                        }
+                )
+            }
+        }
     )
     if(value.isEmpty()) {
         val focusManager = LocalFocusManager.current
